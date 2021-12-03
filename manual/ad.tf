6d3ddd97721e6bb2b429a1a8178b5a31a43e62fd
Aris Kourtesas <ariskourt@gmail.com>
2021-12-03 12:52:07 +0200

    fixing foreach loop

--- manual/ad.tf
+++ manual/ad.tf
@@ -1,5 +1,5 @@
 resource "azuread_user" "user_principal" {
-  for_each = local.users
+  for_each = { for user in local.users : user.user_principal_name => user }
 
   user_principal_name = each.value.user_principal_name
   display_name        = each.value.display_name
@@ -8,14 +8,13 @@ resource "azuread_user" "user_principal" {
 }
 
 resource "random_password" "user_random_password" {
-  for_each = local.users
   length  = 12
   special = false
 }
 
 resource "null_resource" "password_writer" {
-  for_each = local.users
+  for_each = { for user in local.users : user.user_principal_name => user }
   provisioner "local-exec" {
-    command = "echo \"${each.value.display_name} ${random_password.user_random_password.result}\" >> passwords.txt"
+    command = "echo \"${each.value.display_name} ${azuread_user.user_principal[each.value.user_principal_name].password}\" >> passwords.txt"
   }
 }
